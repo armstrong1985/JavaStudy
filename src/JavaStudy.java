acae8693959c21cef15d80b8371be4f36fd0efe0
armstrong1985 <armstrong1985@gmail.com>
2015-04-01 18:39:00 +0300

    Add run speed test to main

--- src/JavaStudy.java
+++ src/JavaStudy.java
@@ -4,16 +4,19 @@ import Work.BoolArrayToBytes;
 public class JavaStudy {
 
     public static void main(String[] args){
-        /** Комментарий к классу TJ0201_HelloDate */
+        long start = System.currentTimeMillis();
         //TJ0312_URShift.main();
-        boolean[] b = new boolean[23];
+        boolean[] b = new boolean[64];
         //b[0]=true; b[1]=true; b[2]=true; b[3]=true; b[4]=true; b[5]=true; b[6]=true; b[7]=true;
         //b[8]=true; b[9]=true; b[10]=true; b[11]=true; b[12]=true; b[13]=true; b[14]=false; b[15]=false;
-        //byte[] B = new byte[ ((b.length)/8 + ((b.length%8==0)?0:1) ) ];
-        byte[] B = BoolArrayToBytes.boolArrayToBytes(b);
 
-        System.out.println("byte[] B is:");
-        for (int i=0; i<B.length ; i++)
-            System.out.println("B["+i+"] = " + Byte.toString(B[i]));
+        //byte[] B = BoolArrayToBytes.boolArrayToBytes(b);
+        for (int j = 0; j < 1000; j++)
+            for (int k = 0; k < 1000; k++)
+                BoolArrayToBytes.boolArrayToBytes(b);
+//        System.out.println("byte[] B is:");
+//        for (int i=0; i<B.length ; i++)
+//            System.out.println("B["+i+"] = " + Byte.toString(B[i]));
+            System.out.println("Run time is " + (System.currentTimeMillis() - start) + " ms");
     }
 }
--- src/Work/BoolArrayToBytes.java
+++ src/Work/BoolArrayToBytes.java
@@ -7,7 +7,7 @@ public class BoolArrayToBytes {
 
         // Calculate value of corresponding bits and "insert" it to byte (using rule 2^b0+2^b1+..2^b7)
         for (int i=0; i<boolArr.length; i++)
-            byteArr[(i/8)] += ( (boolArr[i]) ? ((byte)Math.pow(2,(i%8))) : 0 );     // This part can be optimized using SHIFT and LOGICAL OR operation
+                byteArr[(i/8)] += ( (boolArr[i]) ? ((byte)Math.pow(2,(i%8))) : 0 );     // This part can be optimized using SHIFT and LOGICAL OR operation
 
         return byteArr;
     }
